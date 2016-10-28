df60045a8738330c890e6e940044d42e3bf7ee0f
HY_RORRE <kobitneyjr@gmail.com>
2016-10-28 09:46:26 +0900

    Optimize parsing bms

--- CrossChronox/Score/BmsLoader.cpp
+++ CrossChronox/Score/BmsLoader.cpp
@@ -52,15 +52,25 @@ std::array<std::string,6> difficulty_str = {
 
 
 bool BmsLoader::TryParseObjLine(){
-	//先頭が'#nnncc'形式か判定
+	//0123456
+	//#nnncc:001122
+	const char* comma_pos = nowline + 6;
+	if(*comma_pos != ':'){
+		//'#nnncc:'でないので処理不能
+		return false;
+	}
 	for(int i = 1; i <= 5; ++i){
 		if(!std::isdigit(nowline[i])){
-			//'#nnncc'でないので処理不能
+			//'#nnncc:'でないので処理不能
 			return false;
 		}
 	}
+	if(!parse_nextline_flag){
+		return false;
+	}
 	
 	//TODO: implement
+	return true;
 }
 
 bool BmsLoader::TryParseHeaderLine(){
@@ -78,7 +88,26 @@ bool BmsLoader::TryParseHeaderLine(){
 	}
 	//Other Headers
 	else if(parse_nextline_flag){
-		if(boost::istarts_with(header, "TITLE")){
+		//読み込みを高速化するため、定義が出てきやすいヘッダーを先に判定する
+		//WAVやBPM,LNOBJは複数個出てきやすい
+		if(boost::istarts_with(header, "WAV")){
+			out->sound_channels[GetIndex()].name = GetArg();
+		}
+		else if(boost::istarts_with(header, "BPM")){
+			//0123456
+			//BPM 130
+			//BPM01 130
+			if(std::isblank(header[3])){
+				out->info.init_bpm = atof(GetArg());
+			}
+			else{
+				exbpm[GetIndex()] = atof(GetArg());
+			}
+		}
+		else if(boost::istarts_with(header, "LNOBJ")){
+			lnobj.emplace_back(std::stoi(GetArg(), nullptr, 36));
+		}
+		else if(boost::istarts_with(header, "TITLE")){
 			out->info.title = GetArg();
 		}
 		else if(boost::istarts_with(header, "SUBTITLE")){
@@ -110,29 +139,12 @@ bool BmsLoader::TryParseHeaderLine(){
 		else if(boost::istarts_with(header, "PREVIEWMUSIC")){
 			out->info.preview_music = GetArg();
 		}
-		else if(boost::istarts_with(header, "WAV")){
-			out->sound_channels[GetIndex()].name = GetArg();
-		}
 		else if(boost::istarts_with(header, "PLAYLEVEL")){
 			out->info.level = atoi(GetArg());
 		}
 		else if(boost::istarts_with(header, "DIFFICULTY")){
 			out->info.difficulty = atoi(GetArg());
 			out->info.chart_name = difficulty_str[out->info.difficulty];
-		}
-		else if(boost::istarts_with(header, "BPM")){
-			//0123456
-			//BPM 130
-			//BPM01 130
-			if(std::isblank(header[3])){
-				out->info.init_bpm = atof(GetArg());
-			}
-			else{
-				exbpm[GetIndex()] = atof(GetArg());
-			}
-		}
-		else if(boost::istarts_with(header, "LNOBJ")){
-			lnobj.emplace_back(std::stoi(GetArg(), nullptr, 36));
 		}
 		else if(boost::istarts_with(header, "RANK")){
 			int i = boost::algorithm::clamp(atoi(GetArg()), 0, 3);
@@ -152,8 +164,7 @@ bool BmsLoader::ParseLine(){
 		else return false;
 	}
 	
-	//順序の入れ替え//////////////////
-	if(TryParseHeaderLine() || TryParseObjLine()){
+	if(TryParseObjLine() || TryParseHeaderLine()){
 		//Succeed to parse
 		return true;
 	}
