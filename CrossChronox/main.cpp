1f1c1ce5b94f850d9cc79dac7c049fbe18f51c00
hyrorre <hiromu.yamashita@hotmail.com>
2025-02-17 12:36:04 +0900

    Update: windows project (WinMain)

--- CrossChronox/main.cpp
+++ CrossChronox/main.cpp
@@ -1,7 +1,14 @@
 ﻿#include "Application.hpp"
 #include "Filesystem/Path.hpp"
 
+#if !defined(_WIN64) && !defined(_WIN32) // if not Windows
 int main(int argc, char* argv[]) {
+#else // if Windows
+#include <windows.h>
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd) {
+    int argc = __argc;
+    char** argv = __argv;
+#endif
     Application app(argc, argv);
     try {
         app.Init();
